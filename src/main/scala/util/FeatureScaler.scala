dbd30fae758fd1a0fd1d5c0c0987987781dca6f6
Kelvin Chappell <kelvin.chappell@theguardian.com>
2019-04-16 15:50:07 +0100

    Do week 2 and week 3

--- src/main/scala/util/FeatureScaler.scala
+++ src/main/scala/util/FeatureScaler.scala
@@ -2,17 +2,19 @@ package util
 
 import model.SimplePoint
 
-import scala.annotation.tailrec
-
-
 object FeatureScaler {
 
   def scaledDividedByMax(data: List[SimplePoint]): List[SimplePoint] = ???
 
   def minMaxScaledData(data: List[SimplePoint]): List[SimplePoint] = ???
 
+  def avg(nums: List[Double]): Double = nums.sum / nums.length
 
-  def avg(nums: List[Double]): Double = ???
-
-  def meanNormalisedData(data: List[SimplePoint]): List[SimplePoint] = ???
+  def meanNormalisedData(data: List[SimplePoint]): List[SimplePoint] = {
+    val mean = avg(data.map(_.x))
+    val range = data.map(_.x).max - data.map(_.x).min
+    data.map { point =>
+      SimplePoint((point.x - mean) / range, point.y)
+    }
+  }
 }
--- src/main/scala/util/LinearErrorCalculator.scala
+++ src/main/scala/util/LinearErrorCalculator.scala
@@ -1,12 +1,17 @@
 package util
 
+import breeze.numerics.pow
 import model.SimplePoint
-
 
 object LinearErrorCalculator {
 
-  def linearMeanSquaredError(data: List[SimplePoint], theta0: Double, theta1: Double): Double = ???
-
-  //needed in week 2
-  def linearMeanAbsoluteError(data: List[SimplePoint], theta0: Double, theta1: Double): Double = ???
+  def linearMeanSquaredError(
+      data: List[SimplePoint],
+      theta0: Double,
+      theta1: Double
+  ): Double = {
+    data
+      .map(point => pow((theta0 + theta1 * point.x) - point.y, 2))
+      .sum / (2 * data.length)
+  }
 }
